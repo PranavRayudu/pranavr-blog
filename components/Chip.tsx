4b09f40425c14171db471ded000f02df7580ce4a
Pranav Rayudu <tarakapranav@gmail.com>
2023-09-09 17:33:19 -0700

    added proper filtering to blog page

--- components/Chip.tsx
+++ components/Chip.tsx
@@ -1,21 +1,33 @@
 import Link from 'next/link'
 interface Props {
   text: string
-  href: string
-  highlight: boolean
+  onClick?: () => void
+  highlight?: boolean
+  disabled?: boolean
 }
 
-const Chip = ({ text, href, highlight }: Props) => {
+const Chip = ({ text, onClick, highlight, disabled }: Props) => {
+  if (disabled && highlight) throw new Error('invalid Chip state')
   const colors = highlight
     ? `bg-secondary-300 raised-glow-md-secondary-500 dark:bg-secondary-500 dark:dark-raised-glow-md-secondary-500 border border-opacity-60 border-secondary-400 dark:border-secondary-700`
-    : `text-gray-600 bg-gray-100 raised-glow-sm-gray-100 dark:text-gray-500 dark:bg-gray-800 dark:dark-raised-glow-md-gray-800 border border-opacity-60 border-gray-400 dark:border-gray-700`
+    : `text-gray-600 bg-gray-200 raised-glow-sm-gray-100 dark:text-gray-500 dark:bg-gray-800 dark:dark-raised-glow-md-gray-800 border border-opacity-60 border-gray-400 dark:border-gray-700`
+  if (disabled) {
+    return (
+      <button
+        disabled={disabled}
+        className={`whitespace-nowrap rounded-full px-2 py-0.5 block cursor-not-allowed text-gray-400 bg-gray-100 dark:text-gray-700 dark:bg-gray-800  border border-opacity-60 border-gray-400 dark:border-gray-700`}
+      >
+        {text}
+      </button>
+    )
+  }
   return (
-    <Link
-      href={href}
+    <button
+      onClick={onClick}
       className={`whitespace-nowrap text-black rounded-full px-2 py-0.5 block transition-transform hover:-translate-y-0.5 focus:!drop-shadow-none focus:translate-y-0 ${colors}`}
     >
       {text}
-    </Link>
+    </button>
   )
 }
 
--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 'use client'
 
-import { usePathname } from 'next/navigation'
-import { slug } from 'github-slugger'
+// import { usePathname } from 'next/navigation'
+import GithubSlugger, { slug } from 'github-slugger'
 import { formatDate } from 'pliny/utils/formatDate'
 import { CoreContent } from 'pliny/utils/contentlayer'
 import type { Blog } from 'contentlayer/generated'
@@ -13,84 +13,155 @@ import tagData from 'app/tag-data.json'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft, faChevronRight, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import Chip from '@/components/Chip'
-import { useState } from 'react'
+import { queryTypes, useQueryState } from 'next-usequerystate/app'
+import { useMemo } from 'react'
+
+const POSTS_PER_PAGE = 5
 
 interface PaginationProps {
   totalPages: number
   currentPage: number
+  nextPage: () => void
+  prevPage: () => void
 }
 interface ListLayoutProps {
   posts: CoreContent<Blog>[]
-  title: string
-  initialDisplayPosts?: CoreContent<Blog>[]
-  pagination?: PaginationProps
 }
 
-function Pagination({ totalPages, currentPage }: PaginationProps) {
-  const pathname = usePathname()
-  const basePath = pathname.split('/')[1]
-  const prevPage = currentPage - 1 > 0
-  const nextPage = currentPage + 1 <= totalPages
+function Pagination({ totalPages, currentPage, nextPage, prevPage }: PaginationProps) {
+  // const pathname = usePathname()
+  // const basePath = pathname.split('/')[1]
+  const prevPageNum = currentPage - 1 > 0
+  const nextPageNum = currentPage + 1 <= totalPages
 
   return (
     <div className="space-y-2 pb-8 pt-6 md:space-y-5">
       <nav className="flex justify-between">
-        {!prevPage && (
-          <button className="cursor-auto disabled:opacity-50" disabled={!prevPage}>
+        {!prevPageNum && (
+          <button className="cursor-auto disabled:opacity-50" disabled={!prevPageNum}>
             <FontAwesomeIcon icon={faChevronLeft} /> Previous
           </button>
         )}
-        {prevPage && (
-          <Link
-            className={'primary-link'}
-            href={currentPage - 1 === 1 ? `/${basePath}/` : `/${basePath}/page/${currentPage - 1}`}
-            rel="prev"
-          >
+        {prevPageNum && (
+          <button className={'primary-link'} onClick={prevPage} rel="prev">
             <FontAwesomeIcon icon={faChevronLeft} /> Previous
-          </Link>
+          </button>
         )}
         <span>
           {currentPage} of {totalPages}
         </span>
-        {!nextPage && (
-          <button className="cursor-auto disabled:opacity-50" disabled={!nextPage}>
+        {!nextPageNum && (
+          <button className="cursor-auto disabled:opacity-50" disabled={!nextPageNum}>
             Next <FontAwesomeIcon icon={faChevronRight} />
           </button>
         )}
-        {nextPage && (
-          <Link className={'primary-link'} href={`/${basePath}/page/${currentPage + 1}`} rel="next">
+        {nextPageNum && (
+          <button className={'primary-link'} onClick={nextPage} rel="next">
             Next <FontAwesomeIcon icon={faChevronRight} />
-          </Link>
+          </button>
         )}
       </nav>
     </div>
   )
 }
 
-export default function ListLayoutWithTags({
-  posts,
-  title,
-  initialDisplayPosts = [],
-  pagination,
-}: ListLayoutProps) {
-  const [searchValue, setSearchValue] = useState('')
+function getTagCount(posts: CoreContent<Blog>[]) {
+  const tagCount: Record<string, number> = {}
+  posts.forEach((post) => {
+    if (post.tags && post.draft !== true) {
+      post.tags.forEach((tag) => {
+        const formattedTag = GithubSlugger.slug(tag)
+        if (formattedTag in tagCount) {
+          tagCount[formattedTag] += 1
+        } else {
+          tagCount[formattedTag] = 1
+        }
+      })
+    }
+  })
+  return tagCount
+}
 
-  const pathname = usePathname()
-  const tagCounts = tagData as Record<string, number>
-  const tagKeys = Object.keys(tagCounts)
-  const sortedTags = tagKeys.sort((a, b) => tagCounts[b] - tagCounts[a])
+interface FiltersInterface {
+  // pageNumber: number
+  searchTerm: string | null
+  tags: string[]
+}
+function filterPosts(posts: CoreContent<Blog>[], filters: FiltersInterface): CoreContent<Blog>[] {
+  return posts
+    .filter((post) => {
+      if (filters.searchTerm == null) return true
+      const searchContent = post.title + post.summary + post.tags?.join(' ')
+      return searchContent.toLowerCase().includes(filters.searchTerm.toLowerCase())
+    })
+    .filter((post) => {
+      if (filters.tags == null || filters.tags.length == 0) return true
+      // post.draft !== true && post.tags && post.tags.map((t) => slug(t)).includes(tag)
+      return filters.tags.every((t) => post.tags.map((t) => slug(t)).includes(t))
+      // return post.tags.map((t) => slug(t)).every((tag) => filters.tags.includes(tag))
+    })
+}
 
-  const displayPosts = initialDisplayPosts.length > 0 ? initialDisplayPosts : posts
+export default function ListLayoutWithTags({ posts }: ListLayoutProps) {
+  const [pageNumber, setPageNumber] = useQueryState('page', {
+    ...queryTypes.integer.withDefault(1),
+    history: 'push',
+  })
+
+  const [searchTerm, setSearchTerm] = useQueryState('search')
+
+  const [tags, setTags] = useQueryState('tags', queryTypes.array(queryTypes.string).withDefault([]))
+
+  const setSearch = function (value: string) {
+    setSearchTerm(value)
+    if (pageNumber > 1) setPageNumber(1)
+  }
+
+  const nextPage = function () {
+    if (pageNumber < pagination.totalPages) setPageNumber(pageNumber + 1)
+  }
+
+  const prevPage = function () {
+    if (pageNumber > 1) setPageNumber(pageNumber - 1)
+  }
+
+  const toggleTag = function (tag: string) {
+    if (tag == null) return
+    if (tags.includes(tag)) {
+      setTags(tags.filter((x) => x != tag))
+    } else {
+      setTags([...tags, tag])
+    }
+    if (pageNumber > 1) setPageNumber(1)
+  }
+
+  // const filteredPosts = filterPosts(posts, { pageNumber, tags, searchTerm })
+  const filteredPosts = useMemo(
+    () => filterPosts(posts, { tags, searchTerm }),
+    [posts, tags, searchTerm]
+  )
+
+  const displayPosts = filteredPosts.slice(
+    POSTS_PER_PAGE * (pageNumber - 1),
+    POSTS_PER_PAGE * pageNumber
+  )
+
+  const tagCounts = getTagCount(filteredPosts)
+
+  const allTags = Object.keys(tagData)
+
+  const allTagsSorted = allTags.sort((a, b) => tagData[b] - tagData[a])
+
+  const pagination = {
+    currentPage: pageNumber,
+    totalPages: Math.ceil(filteredPosts.length / POSTS_PER_PAGE),
+    nextPage: nextPage,
+    prevPage: prevPage,
+  }
 
   return (
     <>
       <div>
-        {/*<div>*/}
-        {/*  <h1 className="text-3xl font-extrabold leading-9 font-serif text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">*/}
-        {/*    {title}*/}
-        {/*  </h1>*/}
-        {/*</div>*/}
-
         <div className="flex space-x-0 md:space-x-8 lg:space-x-16 flex-col md:flex-row">
           <div className="max-h-screen h-full sm:flex flex-wrap py-5 card rounded-3xl min-w-[280px] md:max-w-[280px]">
             <div className="py-4 px-6">
@@ -102,23 +173,26 @@ export default function ListLayoutWithTags({
                     <input
                       aria-label="Search articles"
                       type="text"
-                      onChange={(e) => setSearchValue(e.target.value)}
+                      onChange={(e) => setSearch(e.target.value)}
                       placeholder="Search articles"
-                      className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
+                      className="block w-full rounded-3xl border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 dark:focus:inset-sm-gray-900 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
+                      value={searchTerm || ''}
                     />
                   </label>
                 </div>
               </div>
 
               <ul className={'space-y-4 space-x-2 -ml-2'}>
-                {sortedTags.map((t) => {
-                  const selected = pathname.split('/tags/')[1] === slug(t)
+                {allTagsSorted.map((t) => {
+                  const selected = tags.includes(t)
+                  const disabled = !(t in tagCounts)
                   return (
                     <li key={t} className={'float-left mt-4 ml-2'}>
                       <Chip
-                        text={`${t} (${tagCounts[t]})`}
-                        href={selected ? `/blog` : `/tags/${slug(t)}`}
-                        highlight={selected}
+                        text={`${t} (${tagCounts[t] || 0})`}
+                        onClick={() => toggleTag(t)}
+                        highlight={!disabled && selected}
+                        disabled={disabled}
                       />
                     </li>
                   )
@@ -159,9 +233,7 @@ export default function ListLayoutWithTags({
                 )
               })}
             </ul>
-            {pagination && pagination.totalPages > 1 && (
-              <Pagination currentPage={pagination.currentPage} totalPages={pagination.totalPages} />
-            )}
+            {pagination && pagination.totalPages > 1 && <Pagination {...pagination} />}
           </div>
         </div>
       </div>
