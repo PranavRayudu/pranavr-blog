bdcbfd7a0f944603505560910acaef1e2dbc7350
Pranav Rayudu <tarakapranav@gmail.com>
2023-09-15 00:22:19 -0700

    refactored mobile nav to be more compact

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,40 @@
+'use client'
+
 import 'css/tailwind.css'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
-import { SearchConfig, SearchProvider } from 'pliny/search'
 import Header from '@/components/Header'
 import SectionContainer from '@/components/SectionContainer'
 import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
-import { Metadata } from 'next'
+import { DM_Serif_Display, PT_Sans } from 'next/font/google'
+
+// Prevent fontawesome from adding its CSS since we did it manually above
+import { config } from '@fortawesome/fontawesome-svg-core'
+import { KBarConfig, KBarSearchProvider } from 'pliny/src/search/KBar'
+import { Action } from 'kbar'
+import { useRouter } from 'next/navigation'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-  Aboreto,
-  Abril_Fatface,
-  Berkshire_Swash,
-  Carter_One,
-  Courgette,
-  DM_Serif_Display,
-  Fugaz_One,
-  Glass_Antiqua,
-  Leckerli_One,
-  Lemon,
-  Libre_Baskerville,
-  Libre_Bodoni,
-  Lilita_One,
-  Lobster,
-  Lobster_Two,
-  Londrina_Solid,
-  Old_Standard_TT,
-  Oleo_Script,
-  Pacifico,
-  Playball,
-  Playfair,
-  Playfair_Display,
-  PT_Sans,
-  Yeseva_One,
-} from 'next/font/google'
-// import localFont from "next/font/local";
+  faCopy,
+  faEnvelope,
+  faFeather,
+  faGears,
+  faHouseChimney,
+  faInfoCircle,
+  faMoon,
+  faSliders,
+  faSun,
+} from '@fortawesome/free-solid-svg-icons'
+import { useTheme } from 'next-themes'
 
 // https://fontawesome.com/v5/docs/web/use-with/react#getting-font-awesome-css-to-work
 // The following import prevents a Font Awesome icon server-side rendering bug,
 // where the icons flash from a very large icon down to a properly sized one:
 import '@fortawesome/fontawesome-svg-core/styles.css'
-// Prevent fontawesome from adding its CSS since we did it manually above:
-import { config } from '@fortawesome/fontawesome-svg-core'
+import { faFacebook, faGithub, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context'
+import headerNavLinks from '@/data/headerNavLinks'
 config.autoAddCss = false /* eslint-disable import/first */
 
 //fugaz, playfair
@@ -60,44 +54,137 @@ const pt_sans = PT_Sans({
   variable: '--font-sans-serif',
 })
 
-export const metadata: Metadata = {
-  metadataBase: new URL(siteMetadata.siteUrl),
-  title: {
-    default: siteMetadata.title,
-    template: `%s | ${siteMetadata.title}`,
-  },
-  description: siteMetadata.description,
-  openGraph: {
-    title: siteMetadata.title,
-    description: siteMetadata.description,
-    url: './',
-    siteName: siteMetadata.title,
-    images: [siteMetadata.socialBanner],
-    locale: 'en_US',
-    type: 'website',
-  },
-  alternates: {
-    canonical: './',
-    types: {
-      'application/rss+xml': `${siteMetadata.siteUrl}/feed.xml`,
+function generateSocials(): Action[] {
+  const socialSection = 'Socials'
+  const socials: Action[] = [
+    {
+      id: siteMetadata.email || '',
+      name: 'Email',
+      section: socialSection,
+      perform: () => {},
+      icon: <FontAwesomeIcon icon={faEnvelope} fixedWidth className={'text-xs'} />,
     },
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+    {
+      id: siteMetadata.github || '',
+      name: 'Github',
+      section: socialSection,
+      perform: () => window.open(siteMetadata.github, '_blank'),
+      icon: <FontAwesomeIcon icon={faGithub} fixedWidth className={'text-xs'} />,
     },
-  },
-  twitter: {
-    title: siteMetadata.title,
-    card: 'summary_large_image',
-    images: [siteMetadata.socialBanner],
-  },
+    {
+      id: siteMetadata.facebook || '',
+      name: 'Facebook',
+      section: socialSection,
+      perform: () => window.open(siteMetadata.facebook, '_blank'),
+      icon: <FontAwesomeIcon icon={faFacebook} fixedWidth className={'text-xs'} />,
+    },
+    {
+      id: siteMetadata.twitter || '',
+      name: 'Twitter',
+      section: socialSection,
+      perform: () => window.open(siteMetadata.twitter, '_blank'),
+      icon: <FontAwesomeIcon icon={faFacebook} fixedWidth className={'text-xs'} />,
+    },
+    {
+      id: siteMetadata.linkedin || '',
+      name: 'LinkedIn',
+      section: socialSection,
+      perform: () => window.open(siteMetadata.linkedin, '_blank'),
+      icon: <FontAwesomeIcon icon={faLinkedin} fixedWidth className={'text-xs'} />,
+    },
+    {
+      id: siteMetadata.youtube || '',
+      name: 'Youtube',
+      section: socialSection,
+      perform: () => window.open(siteMetadata.youtube, '_blank'),
+      icon: <FontAwesomeIcon icon={faYoutube} fixedWidth className={'text-xs'} />,
+    },
+  ]
+
+  return socials.filter((social) => social.id)
+}
+
+function generateNavigation(router: AppRouterInstance): Action[] {
+  const navigationSection = 'Navigation'
+
+  const navIcons = {
+    Home: faHouseChimney,
+    Blog: faFeather,
+    Projects: faGears,
+    About: faInfoCircle,
+  }
+
+  return headerNavLinks.map((nav) => {
+    return {
+      id: nav.title,
+      name: nav.title,
+      section: navigationSection,
+      icon: navIcons[nav.title] && (
+        <FontAwesomeIcon icon={navIcons[nav.title]} fixedWidth className={'text-xs'} />
+      ),
+      perform: () => router.push(nav.href),
+    }
+  })
+}
+
+function MainContent(props: { children: React.ReactNode }) {
+  const router = useRouter()
+  const { theme, setTheme } = useTheme()
+
+  const defaultActions = [
+    {
+      id: 'theme',
+      name: 'Change Theme',
+      keywords: 'Toggle interface theme from light to dark',
+      section: 'General',
+      icon: <FontAwesomeIcon icon={faSliders} fixedWidth className={'text-xs'} />,
+    },
+    {
+      id: 'darkTheme',
+      name: 'Dark',
+      keywords: 'Change interface to dark theme',
+      parent: 'theme',
+      shortcut: ['l'],
+      perform: () => setTheme('dark'),
+      icon: <FontAwesomeIcon icon={faMoon} fixedWidth className={'text-xs'} />,
+    },
+    // { id: 'actionId', name: 'name' },
+    {
+      id: 'lightTheme',
+      name: 'Light',
+      keywords: 'Change interface to light theme',
+      parent: 'theme',
+      shortcut: ['d'],
+      perform: () => setTheme('light'),
+      icon: <FontAwesomeIcon icon={faSun} fixedWidth className={'text-xs'} />,
+    },
+    {
+      id: 'copy',
+      name: 'Copy URL',
+      shortcut: ['c'],
+      perform: () => navigator.clipboard.writeText(location.href),
+      icon: <FontAwesomeIcon icon={faCopy} fixedWidth className={'text-xs'} />,
+    },
+    ...generateNavigation(router),
+    ...generateSocials(),
+  ]
+
+  return (
+    <SectionContainer>
+      <div className="flex h-screen flex-col justify-between font-sans">
+        <KBarSearchProvider
+          kbarConfig={{
+            ...(siteMetadata.search as KBarConfig).kbarConfig,
+            defaultActions: defaultActions,
+          }}
+        >
+          <Header />
+          <main className="mb-auto">{props.children}</main>
+        </KBarSearchProvider>
+        <Footer />
+      </div>
+    </SectionContainer>
+  )
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -119,15 +206,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="bg-gray-200 text-black antialiased dark:bg-stone-900 dark:text-white">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
-          <SectionContainer>
-            <div className="flex h-screen flex-col justify-between font-sans">
-              <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
-                <Header />
-                <main className="mb-auto">{children}</main>
-              </SearchProvider>
-              <Footer />
-            </div>
-          </SectionContainer>
+          <MainContent>{children}</MainContent>
         </ThemeProviders>
       </body>
     </html>
